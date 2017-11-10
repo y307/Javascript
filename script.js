a6379ae59b78bde3312552e9d77158b480ac4348
y307 <y307@ukr.net>
2017-11-10 14:48:09 +0200

    201711101448

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 jQuery('document').ready(function(){
   jQuery('body').append('<a href="http://www.google.com.ua">Привет Google!</a>');
   jQuery('div').remove();
   jQuery('p').remove();
-  
-=======
-jQuery("document").ready(function(){
-
->>>>>>> c90863540890924db29fc16f37010cf66137882e
-})
+});
