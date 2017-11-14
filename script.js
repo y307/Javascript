b40d067fd504c22d792fef96f3d4f984c03cdd5c
y307 <y307@ukr.net>
2017-11-14 11:14:20 +0200

    20171114@work

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-jQuery('document').ready(function()){
-	jQuery('body').append('<a href="http://google.com">Перейти в Google!</a>');
+jQuery('document').ready(function(){
+  alert('Привет!');
+//	jQuery('body').append('<a href="http://google.com">Перейти в Google!</a>');
 	jQuery('div').remove();
 
-	var katet1, katet2;
-	katet1 = 10;
-	katet2 = 20;
-
-	alert(Math.round(Math.sqrt((katet1*katet1)+(katet2*katet2))))
-}
+});
